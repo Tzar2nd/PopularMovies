62d4c461ff3a52d4773adc566d050331f64a5c44
Ben Gordon Smith <ben_g_smith@msn.com>
2018-02-20 11:32:39 +0000

    Initial commit

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/topzap/android/popularmovies/test/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.topzap.android.popularmovies.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.topzap.android.popularmovies.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from default config.
+  public static final String API_KEY = "fedf0c5b3fc2cef03cfd632e4afe5bd5";
+}
